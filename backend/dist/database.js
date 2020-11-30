fad34c842972183b4c4413bfcaf6eff5112003d4
Hugo Régis <hugonbr@gmail.com>
2020-11-29 22:21:21 -0300

    teste

--- backend/dist/database.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('mysql://root:1234@localhost:3306/swf2020');
-exports.default = sequelize;
