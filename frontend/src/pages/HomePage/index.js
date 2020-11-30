af96bd3c46c5b0659470650d9b22e4e4fa3cf7b2
Hugo Régis <hugonbr@gmail.com>
2020-11-29 23:48:29 -0300

    stats

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -76,7 +76,7 @@ class HomePage extends React.Component {
                                                 <Button variant="outline-info" onClick={() => this.copyToClipboard()} >Copiar</Button>
                                             </InputGroup.Append>
                                         </InputGroup>
-                                        <p>Para acompanhar as estatísticas, acesse {vars.HOST_APP + code}</p>
+                                        <p>Para acompanhar as estatísticas, acesse {vars.HOST_APP + code}/stats</p>
                                     </>
                                 )
                             )}
